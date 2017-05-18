44e272e9273f15070baf41fac9e07aedf344a5c5
novalition <novalition@gmail.com>
2017-05-18 19:56:01 +0700

    Array Multi Dimensi

--- belajar/echobelajar.php
+++ belajar/echobelajar.php
@@ -17,6 +17,12 @@
 	foreach ($resep as $r) { ?>
 		<h1>Cara Membuat Jus <?php echo $r ['buah']?></h1>
 	<p>Campurkan buah <?php echo $r['buah'] ?> sebanyak <?php echo $r ['banyak'] ?> buah ditambahkan dengan <?php echo $r['tambahan'] ?> untuk memberi rasa.
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	 </p>
 	<?php }
 	 ?>
